820f25e073f0545a65f2bc385297202f36fbb1bb
leo.karoubi <leokaroubi@gmail.com>
2020-06-08 23:58:18 +0200

    structure & elf parser lib

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # RVM
+
 rust virtual machine
+
+# Dependencies
+
+# Build
+
+Assured by our servitor `cargo`.
+
+# Run
+
+# Epilogue
+
+To learn rust language.
+
+Feel free to fork, use, improve.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate elf;
+
+use std::path::PathBuf;
+use std::env;
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        2 => (),
+        _ => panic!("Usage: \"cargo run $BINARY\"")
+    }
+    let path = PathBuf::from(&args[1]);
+    let file = elf::File::open_path(&path).expect("Invalid given executable");
     println!("Hello, world!");
 }
