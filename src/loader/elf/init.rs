f3d46c63d87fa3a2e5521bcd9982034bce559c8b
leo.karoubi <leokaroubi@gmail.com>
2020-06-09 10:41:03 +0200

    init elf mod

---
+++ src/loader/elf/init.rs
@@ -0,0 +1,21 @@
+extern crate elf;
+
+use std::path::PathBuf;
+
+pub fn init(file: &String) {
+    let path = PathBuf::from(file);
+    let binobj = elf::File::open_path(&path).expect("Invalid given executable");
+    match binobj.ehdr.machine {
+        elf::types::EM_X86_64 => (),
+        elf::types::EM_AARCH64 => (),
+        elf::types::EM_386 => (),
+        _ => panic!("Invalid target architecture")
+    }
+    match binobj.ehdr.machine {
+        elf::types::EM_X86_64 => (),
+        elf::types::EM_AARCH64 => (),
+        elf::types::EM_386 => (),
+        _ => panic!("Invalid target architecture")
+    }
+    println!("tkt le couz");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-extern crate elf;
-
-use std::path::PathBuf;
 use std::env;
+
+mod loader;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,7 +8,6 @@ fn main() {
         2 => (),
         _ => panic!("Usage: \"cargo run $BINARY\"")
     }
-    let path = PathBuf::from(&args[1]);
-    let file = elf::File::open_path(&path).expect("Invalid given executable");
+    loader::elf::init::init(&args[1]);
     println!("Hello, world!");
 }
