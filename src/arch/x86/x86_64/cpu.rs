adcff40fdd32383190e650b63aafd768f0d99c55
leo.karoubi <leokaroubi@gmail.com>
2020-06-11 13:24:13 +0200

    complete x64 processor

--- src/arch/x86/x86_64/cpu.rs
+++ src/arch/x86/x86_64/cpu.rs
@@ -12,30 +12,61 @@ pub type reg16  = u16;
 pub type reg8   = u8;
 
 /*
- * Struct describing a minimal x64 processor for userspace utilization
+ * Struct describing a minimal  processor for userspace utilization
 */
-pub struct X64UserProc {
+pub struct Proc {
     rax: reg64,
-    rdi: reg64,
-    rsi: reg64,
-    rdx: reg64,
     rbx: reg64,
     rcx: reg64,
-    
-    r8:  reg64,
-    r9:  reg64,
+    rdx: reg64,
+    rsi: reg64,
+    rdi: reg64,
+    rbp: reg64,
+    rsp: reg64,
+    r8: reg64,
+    r9: reg64,
     r10: reg64,
     r11: reg64,
     r12: reg64,
     r13: reg64,
     r14: reg64,
     r15: reg64,
-    
-    rbp: reg64,
-    rsp: reg64,
-    
     rip: reg64,
-
+    rfl: reg64,
+    cpl: reg64,
+    es: reg16,
+    cs: reg16,
+    ss: reg16,
+    ds: reg16,
+    fs: reg16,
+    gs: reg16,
+    ldt: reg64,
+    tr: reg64,
+    gdt: reg64,
+    idt: reg64,
+    cr0: reg32,
+    cr2: reg64,
+    cr3: reg64,
+    cr4: reg32,
+    dr0: reg64,
+    dr1: reg64,
+    dr2: reg64,
+    dr3: reg64,
+    dr6: reg64,
+    dr7: reg64,
+    efer: reg64,
+    fcw: reg64,
+    fsw: reg64,
+    ftw: reg64,
+    mxcsr: reg64,
+    fpr0: reg64,
+    fpr1: reg64,
+    fpr2: reg64,
+    fpr3: reg64,
+    fpr4: reg64,
+    fpr5: reg64,
+    fpr6: reg64,
+    fpr7: reg64,
     xmm00: reg128,
     xmm01: reg128,
     xmm02: reg128,
@@ -54,47 +85,28 @@ pub struct X64UserProc {
     xmm15: reg128,
 }
 
-/*
- * x64 Register list
-*/
-#[derive(Debug, Clone, Copy)]
-pub enum UserRegister {
-    rax,
-    rdi,
-    rsi,
-    rdx,
-    rbx,
-    rcx,
-    r8,
-    r9,
-    r10,
-    r11,
-    r12,
-    r13,
-    r14,
-    r15,
-    rbp,
-    rsp,
-    rip,
-    xmm00,
-    xmm01,
-    xmm02,
-    xmm03,
-    xmm04,
-    xmm05,
-    xmm06,
-    xmm07,
-    xmm08,
-    xmm09,
-    xmm10,
-    xmm11,
-    xmm12,
-    xmm13,
-    xmm14,
-    xmm15,
+#[derive(Copy, Clone, Debug, PartialEq)]
+#[repr(u8)]
+enum RegisterSize {
+    RegSz8   = 0b000,
+    RegSz16  = 0b001,
+    RegSz32  = 0b010,
+    RegSz64  = 0b011,
+    RegSz128 = 0b100,
 }
 
-pub enum KernelRegisters {
+#[derive(Copy, Clone, Debug, PartialEq)]
+#[repr(u8)]
+enum RegisterAccess {
+    RegSys  = 0b0,
+    RegUser = 0b1,
+}
+
+/*
+ *  Register list
+*/
+#[derive(Debug, Clone, Copy)]
+pub enum Register {
     RAX,
     RBX,
     RCX,
@@ -175,85 +187,120 @@ pub enum ProcError {
 
 pub type ProcResult<T> = Result<T, ProcError>;
 
-impl X64UserProc {
+impl Proc {
     /*
-     * Create a x64 processor
+     * Create a  processor
     */
-    pub fn new() -> X64UserProc {
-        X64UserProc {
-            rax:   0x0,
-            rdi:   0x0,
-            rsi:   0x0,
-            rdx:   0x0,
-            rbx:   0x0,
-            rcx:   0x0,
-            r8:    0x0,
-            r9:    0x0,
-            r10:   0x0,
-            r11:   0x0,
-            r12:   0x0,
-            r13:   0x0,
-            r14:   0x0,
-            r15:   0x0,
-            rbp:   0x0,
-            rsp:   0x0,
-            rip:   0x0,
-            xmm00: 0x0,
-            xmm01: 0x0,
-            xmm02: 0x0,
-            xmm03: 0x0,
-            xmm04: 0x0,
-            xmm05: 0x0,
-            xmm06: 0x0,
-            xmm07: 0x0,
-            xmm08: 0x0,
-            xmm09: 0x0,
-            xmm10: 0x0,
-            xmm11: 0x0,
-            xmm12: 0x0,
-            xmm13: 0x0,
-            xmm14: 0x0,
-            xmm15: 0x0,
+    pub fn new() -> Proc {
+        Proc {
+            rax : 0x0,
+            rbx : 0x0,
+            rcx : 0x0,
+            rdx : 0x0,
+            rsi : 0x0,
+            rdi : 0x0,
+            rbp : 0x0,
+            rsp : 0x0,
+            r8 : 0x0,
+            r9 : 0x0,
+            r10 : 0x0,
+            r11 : 0x0,
+            r12 : 0x0,
+            r13 : 0x0,
+            r14 : 0x0,
+            r15 : 0x0,
+            rip : 0x0,
+            rfl : 0x0,
+            cpl : 0x0,
+            es : 0x0,
+            cs : 0x0,
+            ss : 0x0,
+            ds : 0x0,
+            fs : 0x0,
+            gs : 0x0,
+            ldt : 0x0,
+            tr : 0x0,
+            gdt : 0x0,
+            idt : 0x0,
+            cr0 : 0x0,
+            cr2 : 0x0,
+            cr3 : 0x0,
+            cr4 : 0x0,
+            dr0 : 0x0,
+            dr1 : 0x0,
+            dr2 : 0x0,
+            dr3 : 0x0,
+            dr6 : 0x0,
+            dr7 : 0x0,
+            efer : 0x0,
+            fcw : 0x0,
+            fsw : 0x0,
+            ftw : 0x0,
+            mxcsr : 0x0,
+            fpr0 : 0x0,
+            fpr1 : 0x0,
+            fpr2 : 0x0,
+            fpr3 : 0x0,
+            fpr4 : 0x0,
+            fpr5 : 0x0,
+            fpr6 : 0x0,
+            fpr7 : 0x0,
+            xmm00 : 0x0,
+            xmm01 : 0x0,
+            xmm02 : 0x0,
+            xmm03 : 0x0,
+            xmm04 : 0x0,
+            xmm05 : 0x0,
+            xmm06 : 0x0,
+            xmm07 : 0x0,
+            xmm08 : 0x0,
+            xmm09 : 0x0,
+            xmm10 : 0x0,
+            xmm11 : 0x0,
+            xmm12 : 0x0,
+            xmm13 : 0x0,
+            xmm14 : 0x0,
+            xmm15 : 0x0,
         }
     }
 
     /*
      * Get register and returning his value
     */ 
-    pub fn get64_register(&self, reg: UserRegister) -> ProcResult<reg64> {
+    pub fn get64_register(&self, reg: Register) -> ProcResult<reg64> {
         match reg {
-            UserRegister::rax => Ok(self.rax),
-            UserRegister::rdi => Ok(self.rdi),
-            UserRegister::rsi => Ok(self.rsi),
-            UserRegister::rdx => Ok(self.rdx),
-            UserRegister::rbx => Ok(self.rbx),
-            UserRegister::rcx => Ok(self.rcx),
-            UserRegister::r8  => Ok(self.r8),
-            UserRegister::r9  => Ok(self.r9),
-            UserRegister::r10 => Ok(self.r10),
-            UserRegister::r11 => Ok(self.r11),
-            UserRegister::r12 => Ok(self.r12),
-            UserRegister::r13 => Ok(self.r13),
-            UserRegister::r14 => Ok(self.r14),
-            UserRegister::r15 => Ok(self.r15),
-            UserRegister::rbp => Ok(self.rbp),
-            UserRegister::rsp => Ok(self.rsp),
-            UserRegister::rip => Ok(self.rip),
+            Register::RAX => Ok(self.rax),
+            Register::RBX => Ok(self.rbx),
+            Register::RCX => Ok(self.rcx),
+            Register::RDX => Ok(self.rdx),
+            Register::RSI => Ok(self.rsi),
+            Register::RDI => Ok(self.rdi),
+            Register::RBP => Ok(self.rbp),
+            Register::RSP => Ok(self.rsp),
+            Register::R8 => Ok(self.r8),
+            Register::R9 => Ok(self.r9),
+            Register::R10 => Ok(self.r10),
+            Register::R11 => Ok(self.r11),
+            Register::R12 => Ok(self.r12),
+            Register::R13 => Ok(self.r13),
+            Register::R14 => Ok(self.r14),
+            Register::R15 => Ok(self.r15),
+            Register::RIP => Ok(self.rip),
             _ => Err(ProcError::InvalidRegister),
         }
     }
 
-    pub fn get32_register(&self, reg: UserRegister) -> ProcResult<reg32> {
+    pub fn get32_register(&self, reg: Register) -> ProcResult<reg32> {
         let res = self.get64_register(reg)?;
         Ok(res as reg32)
     }
 
-    pub fn get16_register(&self, reg: UserRegister) -> ProcResult<reg16> {
+    pub fn get16_register(&self, reg: Register) -> ProcResult<reg16> {
         let res = self.get64_register(reg)?;
         Ok(res as reg16)
     }
 
-    pub fn get8_register(&self, reg: UserRegister) -> ProcResult<reg8> {
+    pub fn get8_register(&self, reg: Register) -> ProcResult<reg8> {
         let res = self.get64_register(reg)?;
         Ok(res as reg8)
     }
@@ -261,44 +308,44 @@ impl X64UserProc {
     /*
      * Set the register with given value
     */
-    pub fn set64_register(&mut self, reg: UserRegister, val: reg64) -> ProcResult<()> {
+    pub fn set64_register(&mut self, reg: Register, val: reg64) -> ProcResult<()> {
         match reg {
-            UserRegister::rax => { self.rax = val; Ok(()) },
-            UserRegister::rdi => { self.rdi = val; Ok(()) },
-            UserRegister::rsi => { self.rsi = val; Ok(()) },
-            UserRegister::rdx => { self.rdx = val; Ok(()) },
-            UserRegister::rbx => { self.rbx = val; Ok(()) },
-            UserRegister::rcx => { self.rcx = val; Ok(()) },
-            UserRegister::r8  => { self.r8  = val; Ok(()) },
-            UserRegister::r9  => { self.r9  = val; Ok(()) },
-            UserRegister::r10 => { self.r10 = val; Ok(()) },
-            UserRegister::r11 => { self.r11 = val; Ok(()) },
-            UserRegister::r12 => { self.r12 = val; Ok(()) },
-            UserRegister::r13 => { self.r13 = val; Ok(()) },
-            UserRegister::r14 => { self.r14 = val; Ok(()) },
-            UserRegister::r15 => { self.r15 = val; Ok(()) },
-            UserRegister::rbp => { self.rbp = val; Ok(()) },
-            UserRegister::rsp => { self.rsp = val; Ok(()) },
-            UserRegister::rip => { self.rip = val; Ok(()) },
+            Register::RAX => { self.rax = val; Ok(()) },
+            Register::RBX => { self.rbx = val; Ok(()) },
+            Register::RCX => { self.rcx = val; Ok(()) },
+            Register::RDX => { self.rdx = val; Ok(()) },
+            Register::RSI => { self.rsi = val; Ok(()) },
+            Register::RDI => { self.rdi = val; Ok(()) },
+            Register::RBP => { self.rbp  = val; Ok(()) },
+            Register::RSP => { self.rsp  = val; Ok(()) },
+            Register::R8 => { self.r8 = val; Ok(()) },
+            Register::R9 => { self.r9 = val; Ok(()) },
+            Register::R10 => { self.r10 = val; Ok(()) },
+            Register::R11 => { self.r11 = val; Ok(()) },
+            Register::R12 => { self.r12 = val; Ok(()) },
+            Register::R13 => { self.r13 = val; Ok(()) },
+            Register::R14 => { self.r14 = val; Ok(()) },
+            Register::R15 => { self.r15 = val; Ok(()) },
+            Register::RIP => { self.rip = val; Ok(()) },
             _ => Err(ProcError::InvalidRegister),
         }
     }
 
-    pub fn set32_register(&mut self, reg: UserRegister, val: reg32) -> ProcResult<()> {
+    pub fn set32_register(&mut self, reg: Register, val: reg32) -> ProcResult<()> {
         let mut set: reg64 = self.get64_register(reg)?;
         set = (set & ((1 << 32) - 1)) | (val as reg64);
         self.set64_register(reg, set)?;
         Ok(())
     }
 
-    pub fn set16_register(&mut self, reg: UserRegister, val: reg16) -> ProcResult<()> {
+    pub fn set16_register(&mut self, reg: Register, val: reg16) -> ProcResult<()> {
         let mut set: reg64 = self.get64_register(reg)?;
         set = (set & ((1 << 16) - 1)) | (val as reg64);
         self.set64_register(reg, set)?;
         Ok(())
     }
 
-    pub fn set8_register(&mut self, reg: UserRegister, val: reg8) -> ProcResult<()> {
+    pub fn set8_register(&mut self, reg: Register, val: reg8) -> ProcResult<()> {
         let mut set: reg64 = self.get64_register(reg)?;
         set = (set & ((1 << 8) - 1)) | (val as reg64);
         self.set64_register(reg, set)?;
@@ -309,11 +356,11 @@ impl X64UserProc {
 /*
  * supervisor init processor
 */
-pub fn init(stack: reg64, entry: reg64) -> X64UserProc {
-    let mut new = X64UserProc::new();
+pub fn init(stack: reg64, entry: reg64) -> Proc {
+    let mut new = Proc::new();
     new.rbp = stack;
     new.rsp = stack;
     new.rip = entry;
-    println!("x64 Processor initialized");
+    println!(" Processor initialized");
     new
 }
