cb110bfa370f7dbf14fa240bbf1fb95bc2662134
leo.karoubi <leokaroubi@gmail.com>
2020-06-27 19:18:31 +0200

    syscall checkup can now raise errors

--- src/arch/x86/x86_64/syscalls/handlers/exit.rs
+++ src/arch/x86/x86_64/syscalls/handlers/exit.rs
@@ -1,8 +1,10 @@
 use crate::arch::x86::x86_64::scheduler;
 use crate::arch::x86::x86_64::syscalls::syscall;
 
-pub fn syscall_exit(_slf: usize) -> usize {
+use anyhow::Result;
+
+pub fn syscall_exit(_slf: usize) -> Result<usize> {
     let exitcode = syscall::obtain_arg(1);
     scheduler::arch_exit(exitcode as i32);
-    0
+    Ok(0)
 }
--- src/arch/x86/x86_64/syscalls/handlers/write.rs
+++ src/arch/x86/x86_64/syscalls/handlers/write.rs
@@ -1,16 +1,15 @@
 use crate::arch::x86::x86_64::syscalls::syscall;
 use crate::mem::mem;
 
-pub fn syscall_write(slf: usize) -> usize {
+use anyhow::Result;
+
+pub fn syscall_write(slf: usize) -> Result<usize> {
     let a1 = syscall::obtain_arg(1);
     let addr = mem::iftranslation(syscall::obtain_arg(2));
-    match mem::is_segment_readable(addr) {
-        Ok(_k) => (),
-        Err(_e) => return 0,
-    }
+    mem::is_segment_readable(addr)?;
     let a2 = mem::iftranslation(addr);
     let a3 = syscall::obtain_arg(3);
     unsafe {
-        syscall::syscall_exec3(slf, a1, a2, a3)
+        Ok(syscall::syscall_exec3(slf, a1, a2, a3))
     }
 }
